f1d6bfc3b00f1a4b9f15d8c3074de9a1de159647
Adam6615-debug <adamktaher1.0@gmail.com>
2025-02-22 23:34:32 +0200

    assignment 1 web security

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prime', function () {
+    return view('prime');
+});
+Route::get('/even', function () {
+    return view('even');
+});
+Route::get('/multable', function () {
+    return view('multable');
+});
+Route::get('/multable', function () {
+    return view('dosomething');
+});
+Route::get('/multable/{number?}', function ($number = null) {
+    $j = $number??2;
+    return view('multable', compact('j')); //multable.blade.php
+   });
+?>
